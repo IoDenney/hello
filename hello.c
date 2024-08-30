78047d8d9620021edc80e52773e2c5ee1fb59b4e
IoDenney <denneytho@icloud.com>
2024-08-30 18:15:23 -0500

    intial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Denney Thongphet
+ *         dthongphet2@huskers.unl.edu
+ * Date: 2024/08/30
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("I'm Denney Thongphet!\n");
+
+
+  return 0;
+}
