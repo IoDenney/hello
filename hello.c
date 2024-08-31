ba232dec033a843b92a455fbbf31cf6f8ba22874
IoDenney <denneytho@icloud.com>
2024-08-30 22:10:15 -0500

    added my major too

--- hello.c
+++ hello.c
@@ -11,8 +11,15 @@
 
 int main(int argc, char **argv) {
 
+<<<<<<< HEAD
   printf("I'm Denney Thongphet!\n");
   printf("I'm undeclared!\n");
+=======
+  printf("I'm Yatzari Ramirez!\n");
+  printf("My major is Electrical Engineer\n");
+  printf("I'm Denney Thongphet!\n");
+  printf("We are both students!\n");
+>>>>>>> 76d0fb9 (added my major too)
 
 
   return 0;
